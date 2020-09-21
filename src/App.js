a10f1ec97735ea6131afdbe111c0f150467d1b6d
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2020-09-20 22:57:57 -0300

    reactor - change a footer description

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ export default function App(){
                 Direitos de imagem para Netflix.
               </div>
               <div>
-                Dados originzados do site <a href="https://www.themoviedb.org/">Themoviedb.org</a>
+                Dados disponibilizados pela api do <a href="https://www.themoviedb.org/">Themoviedb.org</a>
               </div>
       </footer>
       {movieList.length <= 0 &&
